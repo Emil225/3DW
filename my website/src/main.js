9f99bcbfb0529763ecd82b4960b747bf419d9f2a
Emil225 <emil.m.2503@gmail.com>
2025-02-10 00:29:05 +0000

    i gave a background to my website,the first moving object and also some grid healpers to help

--- my website/src/main.js
+++ my website/src/main.js
@@ -22,3 +22,75 @@ document.querySelector('#app').innerHTML = `
 `
 
 setupCounter(document.querySelector('#counter'))
+
+import * as THREE from 'three';
+
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer({
+  canvas : document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight );
+camera.position.setZ(30);
+camera.position.setX(-3);
+
+renderer.render( scene, camera );
+
+const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
+const material = new THREE.MeshStandardMaterial( { color: 0xFF6347 } );
+const torus = new THREE.Mesh( geometry, material );
+
+scene.add( torus );
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5,5,5);
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
+
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200, 50);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+scene.add(lightHelper, gridHelper);
+
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+
+Array(200).fill().forEach(addStar);
+
+const spaceTexture = new THREE.TextureLoader().load('images/space.jpg');
+scene.background = spaceTexture;
+
+/**
+ * Animate the scene
+ * @param {void} none
+ * @return {void}
+ */
+function animate() {
+  // requestAnimationFrame is a built-in function that schedules the function to be called when the browser is ready to render the next frame
+  requestAnimationFrame( animate );
+
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
+  
+  controls.update();
+  // render the scene
+  renderer.render( scene, camera );
+}
+
+animate();
